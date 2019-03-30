8a162e4c989cbdcd4b8551d8b3f90ce82f3d2289
Anushka Yohan <aliyanage44@gmail.com>
2019-03-30 16:49:23 +0530

    Add request paths for add and get users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const db = require('../database/firebase.js');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  db.getFromCollection('Users', req.query.FirstName)
+    .then(data => {
+      res.status(200).send({ Message: data });
+    })
+    .catch(reject => {
+      res.status(404).send({ Message: reject });
+    });
+});
+
+router.post('/', (req, res, next) => {
+  db.saveToCollection('Users', req.body.FirstName, req.body)
+    .then(event => {
+      res.status(201).send({ Message: 'User created.', Transaction: event });
+    })
+    .catch(reject => {
+      res.status(400).send({ Message: 'Failed to add user.', Error: reject });
+    });
 });
 
 module.exports = router;
