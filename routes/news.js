b798e6f0725deb14736ff4278984bdd32d5c0e92
Anushka Yohan <aliyanage44@gmail.com>
2019-03-30 16:48:56 +0530

    Add request path for fetching news for a specific region

--- routes/news.js
+++ routes/news.js
@@ -4,30 +4,30 @@ const router = express.Router();
 
 /**
  * Configure axios instance with headers.
- * Use this instance for sending HTTP request to another API. 
- * 
+ * Use this instance for sending HTTP request to another API.
+ *
  * Set the following headers.
  *      X-Api-Key: the api key issued by BBC API.
  */
-let x_api_key = 'ca503f961822472b8c20b42fae6db557'
+let x_api_key = 'ca503f961822472b8c20b42fae6db557';
 const axiosInstance = axios.create({
-    baseURL: 'https://newsapi.org/v2',
-    headers: {
-        'X-Api-Key': x_api_key
-    }
+  baseURL: 'https://newsapi.org/v2',
+  headers: {
+    'X-Api-Key': x_api_key,
+  },
 });
 
-
 // headlines.
-router.get('/headlines', function(req, res, next) {
-    let country = req.query.country;
+router.get('/headlines', (req, res, next) => {
+  let country = req.query.country;
 
-    axiosInstance.get(`/top-headlines?country=${country}`)
+  axiosInstance
+    .get(`/top-headlines?country=${country}`)
     .then(resolve => {
-        res.send(resolve.data);
+      res.send(resolve.data);
     })
     .catch(reject => {
-        res.send(reject);
+      res.send(reject);
     });
 });
 
